064c4eae40f195ecc705e5992f9dc2f15bccac7a
wendikardian <wendikardian@gmail.com>
2021-12-29 18:22:52 +0700

    finish

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import Phaser from 'phaser'
 
-import HelloWorldScene from './scenes/HelloWorldScene'
+import AmongUsScene from './scenes/amongUsScene'
 
 const config = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	width: 1920,
+	height: 1080,
 	physics: {
 		default: 'arcade',
 		arcade: {
 			gravity: { y: 200 }
 		}
 	},
-	scene: [HelloWorldScene]
+	scene: [AmongUsScene]
 }
 
 export default new Phaser.Game(config)
